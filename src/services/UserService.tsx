ff57640542d6d19ecd6408cf50208aad202b66ac
hsazzad-prog <hsazzad02@gmail.com>
2024-07-28 12:47:48 +0600

    final commit

--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://192.168.56.1:3000/src/assets/users.json';
+const API_URL = 'https://dummyjson.com/users';
 
 export const fetchUsers = async () => {
     const response = await axios.get(API_URL);
